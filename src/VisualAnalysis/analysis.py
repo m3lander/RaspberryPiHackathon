c0bb089aea18f1e4ddd632c363a17d3c90e1885a
Daniel Ikechukwu <ttdanielik@gmail.com>
2026-01-17 15:06:46 +0000

    boilerplate for analysis

---
+++ src/VisualAnalysis/analysis.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def show_and_tell(img:np.array):
+    """
+    Describes the image returned to function.
+    """
+    pass
+
+def extract_search_terms_from_description(description):
+    """
+    Converts image description into compact search term.
+    """
+
+    pass
