2decd5c3a3ff6762d4e0a6de4317a51f9c04004f
Daniel Ikechukwu <ttdanielik@gmail.com>
2026-01-17 16:06:09 +0000

    updated analysis

--- src/VisualAnalysis/analysis.py
+++ src/VisualAnalysis/analysis.py
@@ -7,8 +7,6 @@ from PIL import Image
 
 load_dotenv()
 
-client = genai.Client()
-
 def show_and_tell(client, img:np.array):
     """
     Describes the image returned to function.
@@ -27,14 +25,14 @@ def show_and_tell(client, img:np.array):
     return response.text
 
 
-def extract_search_terms_from_description(description):
+def extract_search_terms_from_description(client, description):
     """
     Converts image description into compact search term.
     """
 
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
-        contents=["Given the very detailed description, output a more compact description suitable for search purposes in a search engine", description]
+        contents=["Given the very detailed description, output a more compact description suitable for search purposes in a search engine. Your response must be a single sentence.", description]
     )
 
-    return response.text
+    return response.text    
